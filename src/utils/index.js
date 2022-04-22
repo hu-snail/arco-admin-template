7d5c8852470b9293a07daac80b0df97720d520a5
“hu-snail” <1217437592@qq.com>
2022-04-22 14:36:20 +0800

    fix: eslint检测修复

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,43 +23,43 @@ export const isString = (val) => Object.prototype.toString.call(val) === '[objec
  *  判断是否是数字
  * @param {Number} val
  */
-export const checkNum = (val) => /^\d{1,}$/g.test(val)
+export const checkNum = (val) => /^\d{1,}$/g.test(val);
 
 /**
  *  判断是否是字母
  * @param {Number} val
  */
-export const checkLetter = (val) => /^[a-zA-Z]+$/g.test(val)
+export const checkLetter = (val) => /^[a-zA-Z]+$/g.test(val);
 
 /**
  * 判断是否是字母或数字
  * @param {Number || String} val  字符或数字
  */
-export const checkNumOrLetter = (val) => /^[0-9a-zA-Z]*$/g.test(val)
+export const checkNumOrLetter = (val) => /^[0-9a-zA-Z]*$/g.test(val);
 
 /**
  * 判断是否是中文
  * @param {String} data  中文
  */
-export const checkChinese = (val) => /^[\u4E00-\u9FA5]+$/g.test(val)
+export const checkChinese = (val) => /^[\u4E00-\u9FA5]+$/g.test(val);
 
 /**
  * 判断是否是邮箱地址
  * @param {String} val
  */
 export const checkEmail = (val) => {
-  const reg = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g
-  if (reg.test(val)) return true
-}
+  const reg = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g;
+  if (reg.test(val)) return true;
+};
 
 /**
  * 判断是否是手机号，只要是13,14,15,16,17,18,19开头即可
  * @param {String} val
  */
 export const checkTelphone = (val) => {
-  const reg = /^((\+|00)86)?1[3-9]\d{9}$/g
-  if (reg.test(val)) return true
-}
+  const reg = /^((\+|00)86)?1[3-9]\d{9}$/g;
+  if (reg.test(val)) return true;
+};
 
 /**
  * 判断是浏览器内核
@@ -71,8 +71,8 @@ export const checkBrowser = () => {
     presto: u.indexOf('Presto') > -1, // opera内核
     webKit: u.indexOf('AppleWebKit') > -1, // 苹果、谷歌内核
     gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') === -1 // 火狐内核
-  }
-  return Object.keys(obj)[Object.values(obj).indexOf(true)]
+  };
+  return Object.keys(obj)[Object.values(obj).indexOf(true)];
 };
 
 /**
@@ -84,8 +84,8 @@ export const checkIosAndroidIpad = () => {
     ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), // ios终端
     android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, // android终端或者uc浏览器
     iPad: u.indexOf('iPad') > -1 // 是否iPad
-  }
-  return Object.keys(obj)[Object.values(obj).indexOf(true)]
+  };
+  return Object.keys(obj)[Object.values(obj).indexOf(true)];
 };
 
 /**
@@ -95,18 +95,18 @@ export const checkIosAndroidIpad = () => {
  * @param {Object} val 值
  */
 export const localStorageSet = (key, val) => {
-  if (isObject(val)) val = JSON.stringify(val)
-  localStorage.setItem(key, val)
-}
+  if (isObject(val)) val = JSON.stringify(val);
+  localStorage.setItem(key, val);
+};
 
 /**
  * localStorage 获取
  * @param {String} key  属性
  */
-export const localStorageGet = (key) => localStorage.getItem(key)
+export const localStorageGet = (key) => localStorage.getItem(key);
 
 /**
  * localStorage 移除
  * @param {String} key  属性
  */
-export const localStorageRemove = (key) => localStorage.removeItem(key)
+export const localStorageRemove = (key) => localStorage.removeItem(key);
--- src/utils/router.js
+++ src/utils/router.js
@@ -66,7 +66,7 @@ export function setRoutersStore(routerList) {
 }
 
 export function removeRoutersStore() {
-  return localStorage.localStorageRemove('routerList');
+  return localStorageRemove('routerList');
 }
 
 /**
