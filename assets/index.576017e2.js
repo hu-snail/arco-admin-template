474a702617990570e03789b6adf29739e56dcf72
hujiangjun <1217437592@qq.com>
2022-05-05 16:22:18 +0800

    deploy

---
+++ assets/index.576017e2.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.6a349d4e.js";function n(){return a("div",{className:"app-main-container",children:"page two"})}export{n as default};
---
+++ assets/index.97c56d07.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.6a349d4e.js";function n(){return a("div",{className:"app-main-container",children:"page one"})}export{n as default};
