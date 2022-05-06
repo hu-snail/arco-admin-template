6b0eb196226ff527f2f72e0f12311d20d5b46c45
hujiangjun <1217437592@qq.com>
2022-05-06 09:34:05 +0800

    deploy

--- assets/index.8ed0e212.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index.4af42720.js";function n(){return a("div",{className:"app-main-container",children:"page two"})}export{n as default};
---
+++ assets/index.fdecb212.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.5cf7137b.js";function n(){return a("div",{className:"app-main-container",children:"page two"})}export{n as default};
